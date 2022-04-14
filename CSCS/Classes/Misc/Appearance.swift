71ad7b05e56320fc5c7c84c7bb3f01ab3c480e5e
akonst17 <akonst17@gmail.com>
2022-04-14 12:51:37 +0300

    upgrade; build 7

--- CSCS/Classes/Misc/Appearance.swift
+++ CSCS/Classes/Misc/Appearance.swift
@@ -18,4 +18,8 @@ struct Appearance {
     static let progress1Color = UIColor(integralRed: 15, green: 51, blue: 93)
     static let progress2Color = UIColor(integralRed: 253, green: 182, blue: 27)
     static let progress3Color = UIColor(integralRed: 182, green: 80, blue: 55)
+    
+    static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
+    static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
+    static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
 }
